bf0e28e45e826b257c8440aba8c8218023900799
Albert Tang <tangalbert919@yahoo.com>
2023-02-21 11:00:11 -0600

    sm8250-common: Import task_profiles.json
    Change-Id: I922e83abeb47706303f2b83698ea07c09a65b069

--- common.mk
+++ common.mk
@@ -353,6 +353,9 @@ PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Perf
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
+
 PRODUCT_PACKAGES += \
     libqti-perfd-client
 
